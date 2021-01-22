e62c0c8183d5e22e45e09da11182325618caedc8
yus2on <gbwl2784@gmail.com>
2021-01-22 14:09:45 +0900

    feat: Add BoardController and TDD

---
+++ src/main/java/com/fastcampus/board/controller/BoardController.java
@@ -0,0 +1,27 @@
+package com.fastcampus.board.controller;
+
+import com.fastcampus.board.model.Board;
+import com.fastcampus.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardRepository boardRepository;
+    // private final BoardService boardService;
+
+    @GetMapping("/api/posts")
+    public List<Board> getList() {
+        return boardRepository.findAll();
+
+        // 만약 리턴했는데 리스트가 아무것도 없다면 ??
+        // null 체크 -> 예ㅊ외처리 필요
+    }
+
+
+
+}
